ab54e61c7bd272d7ec8023ef0d2db5ea0dc82f03
Mayukh Dev <mayukh.dev@knowlarity.com>
2019-04-14 17:13:15 +0530

    Modifed tournament

--- knowlarity/tournament/models.py
+++ knowlarity/tournament/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Sport(models.Model):
+    SPORT_CHOICE = (
+        ('TT', 'Table Tennis'),
+        ('FB', 'Foos Ball'),
+    )
+    name = models.CharField(max_length=2, choices=SPORT_CHOICE, default='TT')
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=64)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, related_name="sports")
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{} : {} Score: {}".format(self.sport, self.name, self.score)
+
+
+class Employee(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    email_id = models.EmailField(max_length=70, unique=True)
+    team = models.ManyToManyField(Team, blank=True, related_name="team")
+
+    def __str__(self):
+        return "{} {} ({})".format(self.first, self.last, self.email_id)
--- knowlarity/tournament/views.py
+++ knowlarity/tournament/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from .models import Sport, Team, Employee
+from django.http import JsonResponse
+
+
+def index(request):
+    context = {
+        "teams": Team.objects.all()
+    }
+    return render(request, "tournament/index.html", context)
+
+
+def bookscore(request):
+    context = {
+        "employees": Employee.objects.all(),
+        "max_per_team": range(2),
+        "max_team_per_event": range(2)
+    }
+    return render(request, "tournament/bookscore.html", context)
+
+
+def autocomplete(request):
+    if request.is_ajax():
+        queryset = Employee.objects.all()
+        list = []
+        for i in queryset:
+            list.append(str(i))
+        data = {
+            'list': list,
+        }
+        return JsonResponse(data)
