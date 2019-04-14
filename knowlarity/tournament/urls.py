dacf35968409f27c4d069cf7eff3184c517dc5fc
Mayukh Dev <mayukh.dev@knowlarity.com>
2019-04-15 01:23:59 +0530

    Added leadboard-view and modified urls and views

--- knowlarity/tournament/urls.py
+++ knowlarity/tournament/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 
 from . import views
@@ -8,6 +8,8 @@ urlpatterns = [
     path("bookscore", views.bookscore, name="bookscore"),
     path("addscore", views.addscore, name="addscore"),
     path("leaderboard", views.leaderboard, name="leaderboard"),
-    path("leaderboard/<int:sport_id>", views.leaderboard, name="leaderboard"),
+    path('leaderboard/<int:sport_id>', views.leaderboard, name="leaderboard"),
+    path('leaderboard/<int:sport_id>/<int:offset>/', views.leaderboard, name="leaderboard"),
+    path('leaderboard/<int:sport_id>/<int:offset>/<int:limit>/', views.leaderboard, name="leaderboard"),
     url(r'^ajax/autocomplete/$', views.autocomplete, name='ajax_autocomplete')
 ]
